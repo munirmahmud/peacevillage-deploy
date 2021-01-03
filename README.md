03588e08de201dc73484a0e5d1c92e1036a51827
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-03 16:17:52 +0600

    Project Finalized

---
+++ README.md
@@ -0,0 +1,1 @@
+# peacevillage-deploy
---
+++ src/client/precache-manifest.cd543998c6a0565aca46ecbba98419f2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d746038eeff5b2750a73b04c9239ff88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61b1679906c3330af34",
+    "url": "/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "7ec3262f87544cc38804",
+    "url": "/static/css/main.bb292cc6.chunk.css"
+  },
+  {
+    "revision": "f61b1679906c3330af34",
+    "url": "/static/js/2.d6ef06b5.chunk.js"
+  },
+  {
+    "revision": "dd79e072a364e5d20c5b986f5bf1bab2",
+    "url": "/static/js/2.d6ef06b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ec3262f87544cc38804",
+    "url": "/static/js/main.2a83dbf6.chunk.js"
+  },
+  {
+    "revision": "9df4366502d0126cdd5f",
+    "url": "/static/js/runtime-main.2b54a308.js"
+  },
+  {
+    "revision": "cd98e2c39b50532ef09a3344e820499f",
+    "url": "/static/media/cancun.cd98e2c3.jpg"
+  },
+  {
+    "revision": "37789c74d6b086fd2e9638e437283f70",
+    "url": "/static/media/dubai.37789c74.jpg"
+  },
+  {
+    "revision": "14572c1da2ffe2fdf72007141338ac88",
+    "url": "/static/media/hero-bg.14572c1d.jpg"
+  },
+  {
+    "revision": "fb5d70bd26078b18cd754fc63af9c87d",
+    "url": "/static/media/london.fb5d70bd.jpg"
+  },
+  {
+    "revision": "89461b111ba1c5d22c940400d1d30d04",
+    "url": "/static/media/los-angeles.89461b11.jpg"
+  },
+  {
+    "revision": "23aadf8146a0240ddc18f846d8c64843",
+    "url": "/static/media/san-fransisco.23aadf81.jpg"
+  },
+  {
+    "revision": "83e03ad00e51e632a03dda54df295f2c",
+    "url": "/static/media/toronto.83e03ad0.jpg"
+  }
+]);
